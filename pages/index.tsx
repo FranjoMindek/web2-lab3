a7e722aefb86112536739fed548bebd933eaeb2d
Franjo Mindek <franjo.mindek@neos.hr>
2022-11-14 14:11:34 +0100

    template

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Template by FM</title>
+        <meta name="description" content="Template by FM" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
